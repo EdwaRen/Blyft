4a8c37e533adaf6adc29662636a0faae6ea8b5f0
Eddie <eddie.ren.2013@gmail.com>
2019-02-17 04:04:42 -0700

    a

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const fs = require('fs');
+var path = require('path');
 
 const db_cred = JSON.parse(fs.readFileSync('db_credentials.json'));
 // let port = process.env.PORT || 80;
@@ -9,7 +10,7 @@ const db_cred = JSON.parse(fs.readFileSync('db_credentials.json'));
 // mongoose.connect(`mongodb+srv://${db_cred.username}:${db_cred.password}@busroutes-hd8th.mongodb.net/BusPool`, { useNewUrlParser: true });
 mongoose.connect('mongodb://localhost:27017/busroutes', {useNewUrlParser: true});
 
-const User = mongoose.model('user', new mongoose.Schema({
+const User = mongoose.model('users', new mongoose.Schema({
     start: {
         lat: Number,
         lng: Number
@@ -40,29 +41,32 @@ app.get('/addRoute', function(req, res){
 
 
     const { spawn } = require('child_process');
-    const pyProg = spawn('python', ['kmeans.py']);
+    const pyProg = spawn('python3', ['kmeans.py']);
     console.log("pyProg launched");
 
     pyProg.stdout.on('data', function(data) {
         console.log("logging data");
         console.log(data.toString());
-        res.write(data);
+        // res.write(data);
         res.end('end');
     });
     console.log("end detected");
 
-    res.send('done', obj);
 });
 
 app.get('/getRoutes', function(req, res){
     res.send("get routes");
+    res.sendFile(path.join(__dirname + '/index.html'));
+
 });
 
 app.post('/locationRequest', function(req, res){
     console.log(req.body);
     // console.log(req.body[0]);
 
-    res.send("location request")
+    // res.send("location request")
+    res.sendFile(path.join(__dirname + '/index.html'));
+
 });
 
 // app.listen(port);
